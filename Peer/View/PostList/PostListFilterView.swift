959c6e475e01edcbace3224d815ad7ebab499a41
HiHoi <ghtjd5087@gmail.com>
2024-04-11 21:14:36 +0900

    feat: filter view add and sort postlist type add

--- Peer/View/PostList/PostListFilterView.swift
+++ Peer/View/PostList/PostListFilterView.swift
@@ -8,55 +8,88 @@
 import SwiftUI
 
 struct PostListFilterView: View {
-    var body: some View {
-		ZStack {
+	@State private var selectedType: String = "전체"
+	@State private var showFilter: Bool = false
+
+	var body: some View {
+		ZStack(alignment: .leading) {
 			Color.tertiaryBackground
-			VStack {
-				// All / Study / Project
-//				FilterForTypeView(tabIndex: )
-
-				// Filter
-
-				// Sort
+			HStack {
+				// 리스트 분류 선택
+				SelectSortForPost(selectedType: $selectedType)
+				Spacer()
+				// 필터링 버튼
+				Button(action: {
+					showFilter.toggle()
+				}, label: {
+					Text("Button")
+				})
 			}
+			.padding(20)
 		}
-		.frame(height: 100)
-    }
+		.frame(maxHeight: 70)
+		.sheet(isPresented: $showFilter, content: {
+			FilteringBottomSheet()
+		})
+	}
 }
 
-struct FilterTabBarItem: View {
-	@Binding var tabIndex: Int
-	var name: String
+struct FilteringBottomSheet: View {
+	@Environment(\.presentationMode) var presentationMode
 	var body: some View {
-		Button {
-
-		} label: {
+		ZStack {
+			Color.tertiaryBackground
+				.ignoresSafeArea()
 			VStack {
-//				if tabIndex == index {
-//					Text(name)
-//						.font(.pretendardBold24)
-//						.foregroundColor(.white)
-//				} else {
-//					Text(name)
-//						.font(.pretendardBold18)
-//						.foregroundColor(.alternativeText)
-//				}
+				HStack {
+					Spacer()
+					Button(action: {
+						presentationMode.wrappedValue.dismiss()
+					}) {
+						Image(systemName: "xmark")
+					}
+				}
+				.padding(20)
+				Spacer()
+				
+				Spacer()
 			}
 		}
 	}
 }
 
-struct FilterForTypeView: View {
-	@Binding var tabIndex: Int
-	@Namespace var namespace
-	let types = ["전체", "스터디", "프로젝트"]
+struct SortPostItem: View {
+	var name: String
+	var action: () -> Void // 탭 선택 시 수행할 액션
+	var isSelected: Bool = false
 
 	var body: some View {
-		HStack {
+		Button(action: action) {
+			VStack {
+				Text(name)
+					.font(.pretendardBold24)
+					.foregroundColor(
+						isSelected ? .normalText : .alternativeText)
+					.scaleEffect(isSelected ? 1.2 : 1.0)
+					.animation(.easeInOut(duration: 0.3), value: isSelected)
+			}
+		}
+	}
+}
+
+struct SelectSortForPost: View {
+	let types = ["전체", "스터디", "프로젝트"]
+	@Binding var selectedType: String
+
+	var body: some View {
+		HStack(alignment: .bottom, spacing: 20) {
 			ForEach(types, id: \.self) { type in
-				FilterTabBarItem(
-					tabIndex: $tabIndex, 
-					name: type
+				SortPostItem(
+					name: type,
+					action: {
+						selectedType = type
+					},
+					isSelected: selectedType == type
 				)
 			}
 		}
@@ -64,5 +97,5 @@ struct FilterForTypeView: View {
 }
 
 #Preview {
-    PostListFilterView()
+	PostListFilterView()
 }
