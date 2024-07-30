c68cb607abb28086f64c81c2e677a53bfb0b6b58
HiHoi <ghtjd5087@gmail.com>
2024-07-30 20:11:26 +0900

    feat: setting view

--- Peer/View/LoginView.swift
+++ Peer/View/LoginView.swift
@@ -8,19 +8,132 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-		ZStack {
-            Color(.primaryBackground)
-                .ignoresSafeArea()
-            
-            VStack {
-                Text("로그인")
-                    .font(.pretendardBold28)
+        GeometryReader { geometry in
+            ZStack {
+                Color(.primaryBackground)
+                    .ignoresSafeArea()
                 
-                
+                VStack {
+                    VStack {
+                        Text("로그인")
+                            .font(.pretendardBold28)
+                            .padding()
+                        
+                        TextField(
+                            "이메일을 입력해주세요.",
+                            text: $email
+                        )
+                        .padding(10)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        
+                        SecureField(
+                            "비밀번호를 입력해주세요.",
+                            text: $password
+                        )
+                        .padding(10)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        
+                        HStack {
+                            Spacer()
+                            Button(action: {}, label: {
+                                Text("비밀번호 찾기")
+                            })
+                            .padding(.horizontal, 10)
+                        }
+                        
+                        PurpleButton(
+                            disable: email.isEmpty || password.isEmpty,
+                            text: "로그인",
+                            action: {}
+                        )
+                        .padding()
+                    }
+                    .frame(width: geometry.size.width - 100)
+                    .padding()
+                    .background(Color.secondaryBackground)
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 15)
+                    )
+                    
+                    VStack {
+                        Divider()
+                            .frame(height: 3)
+                            .overlay(Color.gray.opacity(0.4))
+                            .padding(20)
+                        
+                        FourtyTwoAuthButton(width: geometry.size.width - 100)
+                        GoogleAuthButton(width: geometry.size.width - 100)
+                    }
+                }
             }
+        }
+    }
+}
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.tertiaryBackground)
+                .cornerRadius(8)
+                .frame(height: 46)
             
-		}
+            configuration
+                .font(.pretendardMedium18)
+                .padding()
+        }
+    }
+}
+
+struct GoogleAuthButton: View {
+    var width: CGFloat
+    var body: some View {
+        Button(
+            action: {
+                // TODO : - 구글 Oauth 붙이기
+            },
+            label: {
+                HStack {
+                    Image("googleLogo")
+                    
+                    Text("구글 계정으로 로그인")
+                        .font(.pretendardBold18)
+                }
+            }
+        )
+        .frame(width: width, height: 50)
+        .background(Color.white)
+        .clipShape(
+            RoundedRectangle(cornerRadius: 15)
+        )
+    }
+}
+
+struct FourtyTwoAuthButton: View {
+    var width: CGFloat
+    var body: some View {
+        Button(
+            action: {
+                // TODO : - 42 Oauth 붙이기
+            },
+            label: {
+                HStack {
+                    Image("42Logo")
+                    
+                    Text("42 계정으로 로그인")
+                        .font(.pretendardBold18)
+                }
+            }
+        )
+        .frame(width: width, height: 50)
+        .background(Color.white)
+        .clipShape(
+            RoundedRectangle(cornerRadius: 15)
+        )
     }
 }
 
--- Peer/View/Setting/SettingView.swift
+++ Peer/View/Setting/SettingView.swift
@@ -7,9 +7,174 @@
 
 import SwiftUI
 
-struct SettingView: View {
+struct SettingHeaderView: View {
     var body: some View {
-        Text("Hello, SettingView")
+        HStack {
+            Text("설정페이지")
+                .font(.pretendardBold24)
+            
+            Spacer()
+        }
+    }
+}
+
+struct Profile: View {
+    var body: some View {
+        VStack {
+            Text("View 1")
+        }
+    }
+}
+
+struct View2: View {
+    var body: some View {
+        VStack {
+            Text("View 2")
+        }
+    }
+}
+
+struct View3: View {
+    var body: some View {
+        VStack {
+            Text("View 3")
+        }
+    }
+}
+
+struct View4: View {
+    var body: some View {
+        VStack {
+            Text("View 4")
+        }
+    }
+}
+
+struct View5: View {
+    var body: some View {
+        VStack {
+            Text("View 5")
+        }
+    }
+}
+
+struct SettingView: View {
+    @State var currentTab: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color("PrimaryBackground")
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+                SettingHeaderView()
+                    .padding(20)
+                
+                TabBarView(currentTab: self.$currentTab)
+                    .frame(height: 80)
+                
+                ZStack {
+                    Color.tertiaryBackground
+                    
+                    TabView(selection: self.$currentTab) {
+                        Profile().tag(0)
+                        View2().tag(1)
+                        View3().tag(2)
+                        View4().tag(3)
+                        View5().tag(4)
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                    .edgesIgnoringSafeArea(.all)
+                    
+                }
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+                
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+struct TabBarView: View {
+    @Binding var currentTab: Int
+    @Namespace var namespace
+    
+    var tabBar: [(String, String)] = [
+            ("내 정보", "person.fill"),
+            ("내 관심", "heart.fill"),
+            ("쪽지", "bubble.left.fill"),
+            ("개인정보", "lock.fill"),
+            ("설정", "gearshape.fill")
+        ]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            HStack(alignment: .center, spacing: 10) {
+                ForEach(
+                    Array(zip(self.tabBar.indices,
+                              self.tabBar)),
+                    id: \.0,
+                    content: {
+                        index, item in
+                        TabBarItem(
+                            currentTab: self.$currentTab,
+                            namespace: namespace.self,
+                            tabBarItemName: item.0,
+                            tabBarItemIcon: item.1,
+                            tab: index)
+                        .frame(width: (geometry.size.width - 70) / CGFloat(tabBar.count))
+                    })
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct TabBarItem: View {
+    @Binding var currentTab: Int
+    let namespace: Namespace.ID
+    
+    var tabBarItemName: String
+    var tabBarItemIcon: String
+    var tab: Int
+    
+    var body: some View {
+        ZStack {
+            if currentTab == tab {
+                Color.strongPurple
+            } else {
+                Color.secondaryBackground
+            }
+            
+            Button {
+                self.currentTab = tab
+            } label: {
+                VStack {
+                    if currentTab == tab {
+                        VStack {
+                            Image(systemName: tabBarItemIcon)
+                                .padding(3)
+                            Text(tabBarItemName)
+                        }
+                        .fontWeight(.bold)
+                        
+                    } else {
+                        VStack {
+                            Image(systemName: tabBarItemIcon)
+                                .foregroundColor(Color.gray)
+                                .padding(3)
+                            Text(tabBarItemName)
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
+                .animation(.spring(), value: self.currentTab)
+            }
+            .buttonStyle(.plain)
+            
+        }
+        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
     }
 }
 
